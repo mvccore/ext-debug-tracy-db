1056b039a4f1076137798a366fa0798e3031bfc8
Tom Flídr <tomflidr@gmail.com>
2021-07-21 17:17:38 +0200

    Stack trace rendering and rows collapse saved states.

--- src/MvcCore/Ext/Debugs/Tracys/DbPanel.php
+++ src/MvcCore/Ext/Debugs/Tracys/DbPanel.php
@@ -15,7 +15,7 @@ namespace MvcCore\Ext\Debugs\Tracys;
 
 /**
  * Responsibility - dump all executed SQL queries on configured connection(s) 
- *                  with params, exec times and stack traces.
+ *                  with params, exec time, stack trace and connection name.
  */
 class DbPanel implements \Tracy\IBarPanel {
 
@@ -31,6 +31,18 @@ class DbPanel implements \Tracy\IBarPanel {
 	 * @var array|NULL
 	 */
 	protected $queries = NULL;
+
+	/**
+	 * Executed queries count.
+	 * @var int
+	 */
+	protected $queriesCount = 0;
+
+	/**
+	 * Executed queries total time.
+	 * @var float
+	 */
+	protected $queriesTime = 0.0;
 
 	/**
 	 * Get unique `Tracy` debug bar panel id.
@@ -57,14 +69,14 @@ class DbPanel implements \Tracy\IBarPanel {
 	 */
 	public function getPanel() {
 		$this->prepareQueriesData();
-		if (count($this->queries) === 0) return '';
+		if ($this->queriesCount === 0) return '';
 		ob_start();
 		include(__DIR__ . '/db.panel.phtml');
 		return ob_get_clean();
 	}
 
 	/**
-	 * 
+	 * Prepare view data for rendering.
 	 * @return void
 	 */
 	protected function prepareQueriesData () {
@@ -73,20 +85,123 @@ class DbPanel implements \Tracy\IBarPanel {
 		$sysConfProps = \MvcCore\Model::GetSysConfigProperties();
 		$dbDebugger = \MvcCore\Ext\Models\Db\Debugger::GetInstance();
 		$store = & $dbDebugger->GetStore();
+		$appRoot = \MvcCore\Application::GetInstance()->GetRequest()->GetAppRoot();
+		$appRootLen = mb_strlen($appRoot);
 		foreach ($store as $item) {
 			$connection = $item->connection;
 			$connConfig = $connection->GetConfig();
 			list(
 				$dumpSuccess, $queryWithValues
-			) = \MvcCore\Ext\Models\Db\Connection::DumpQueryWithParams($connection->GetProvider(), $item->query, $item->params);
+			) = \MvcCore\Ext\Models\Db\Connection::DumpQueryWithParams(
+				$connection->GetProvider(), $item->query, $item->params
+			);
+			$preparedStack = $this->prepareStackData($item->stack, $appRoot, $appRootLen);
 			$this->queries[] = (object) [
 				'query'		=> $dumpSuccess ? $queryWithValues : $item->query,
 				'params'	=> $dumpSuccess ? $item->params : NULL,
 				'exec'		=> $item->exec,
-				'stack'		=> $item->stack,
+				'execMili'	=> $item->exec * 1000,
+				'stack'		=> $preparedStack,
 				'connection'=> $connConfig->{$sysConfProps->name},
+				'hash'		=> $this->hashQuery($item, $preparedStack),
 			];
+			$this->queriesTime += $item->exec;
 		}
+		$this->queriesCount = count($this->queries);
+		$this->queriesTime = $this->queriesTime;
 		$dbDebugger->Dispose();
+	}
+
+	/**
+	 * Prepare code for stack trace rendering.
+	 * @param  array  $stack 
+	 * @param  string $appRoot 
+	 * @param  int    $appRootLen 
+	 * @return \string[][]
+	 */
+	protected function prepareStackData (array $stack, $appRoot, $appRootLen) {
+		$result = [];
+		foreach ($stack as $stackItem) {
+			$file = NULL;
+			$line = NULL;
+			$class = NULL;
+			$func = NULL;
+			$callType = '';
+			if (isset($stackItem['file']))
+				$file = str_replace('\\', '/', $stackItem['file']);
+			if (isset($stackItem['line']))
+				$line = $stackItem['line'];
+			if (isset($stackItem['class']))
+				$class = $stackItem['class'];
+			if (isset($stackItem['function']))
+				$func = $stackItem['function'];
+			if (isset($stackItem['type']))
+				$callType = str_replace('-', '&#8209;', $stackItem['type']);
+			$callType = '::';
+			if ($func !== NULL && $file !== NULL && $line !== NULL) {
+				$visibleFilePath = $this->getVisibleFilePath($file, $appRoot, $appRootLen);
+				$phpCode = $class !== NULL
+					? $class . $callType . $func . '();'
+					: $func . '();';
+				$link = \Tracy\Helpers::editorUri($file, $line);
+				$result[] = [
+					'<a title="'.$file.':'.$line.'" href="'.$link.'">'.$visibleFilePath.':'.$line.'</a>',
+					$phpCode
+				];
+			} else {
+				$result[] = [
+					NULL,
+					$class !== NULL
+						? $class . $callType . $func . '();'
+						: $func . '();'
+				];
+			}
+		}
+		return $result;
+	}
+
+	/**
+	 * Return file path to render in link text.
+	 * If there is found application root in path, 
+	 * return only path after it, if not, return 
+	 * three dots, two parent folders and filename.
+	 * @param  string $file 
+	 * @param  string $appRoot 
+	 * @param  int    $appRootLen 
+	 * @return string
+	 */
+	protected function getVisibleFilePath ($file, $appRoot, $appRootLen) {
+		$result = $file;
+		if (mb_strpos($file, $appRoot) === 0) {
+			$result = mb_substr($file, $appRootLen);
+		} else {
+			$i = 0;
+			$pos = mb_strlen($file) + 1;
+			while ($i < 3) {
+				$pos = mb_strrpos(mb_substr($file, 0, $pos - 1), '/');
+				if ($pos === FALSE) break; 
+				$i++;
+			}
+			if ($pos === FALSE) {
+				$result = $file;
+			} else {
+				$result = '&hellip;'.mb_substr($file, $pos);
+			}
+		}
+		return $result;
+	}
+
+	/**
+	 * Create unique query MD5 hash.
+	 * @param  \stdClass   $item 
+	 * @param  \string[][] $preparedStack 
+	 * @return string
+	 */
+	protected function hashQuery ($item, $preparedStack) {
+		return md5(implode('', [
+			$item->query,
+			serialize($item->params),
+			serialize($preparedStack)
+		]));
 	}
 }
