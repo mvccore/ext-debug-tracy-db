cb0bccd3e00052f798597a9b86e28b315cb5c8ed
Tom Flídr <tomflidr@gmail.com>
2024-12-09 17:27:26 +0100

    release v5.3.1

--- src/MvcCore/Ext/Debugs/Tracys/DbPanel.php
+++ src/MvcCore/Ext/Debugs/Tracys/DbPanel.php
@@ -25,7 +25,7 @@ class DbPanel implements \Tracy\IBarPanel {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.3.0';
+	const VERSION = '5.3.1';
 	
 	/**
 	 * Query type keywords to match.
